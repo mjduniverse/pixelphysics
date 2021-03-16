65f897c01302d4c48ec6af9510867c92ee73ed03
Michael J. Dole <dev@mjduniverse.com>
2021-03-16 17:59:16 -0500

    Moving code

---
+++ src/createPhSimInstance.js
@@ -0,0 +1,133 @@
+
+/**
+ * Create PhSim instance
+ * 
+ * @function
+ * @memberof PPGSploderEmulator.prototype
+ * @this PPGSploderEmulator
+ */
+
+function createPhSimInstance() {
+
+    var staticSim = {
+
+        simulations: [],
+
+        box: {
+            w: 640,
+            h: 480
+        }
+
+    }
+
+    for(let i = 0; i < this.levels.length; i++) {
+
+        let level = {
+
+            data: {
+                sploder: this.levels[i]
+            },
+
+            world: {},
+
+            layers: [
+                {
+                    objUniverse: [],
+                    name: "Layer 5"
+                },
+
+                {
+                    objUniverse: [],
+                    name: "Layer 4"
+                },
+
+                {
+                    objUniverse: [],
+                    name: "Layer 3"
+                },
+
+                {
+                    objUniverse: [],
+                    name: "Layer 2"
+                },
+
+                {
+                    objUniverse: [],
+                    name: "Layer 1"
+                },
+
+                {
+                    objUniverse: [],
+                    name: "bounds"
+                }
+
+            ]
+        };
+
+        let topRect = {
+            shape: "rectangle",
+            collisionClass: "0 1 2 3 4",
+            locked: true,
+            x: 0,
+            y: 0,
+            w: staticSim.box.w,
+            h: 10
+        }
+
+        let botomRect = {
+            shape: "rectangle",
+            collisionClass: "0 1 2 3 4",
+            locked: true,
+            x: 0,
+            y: staticSim.box.h,
+            w: staticSim.box.w,
+            h: 10
+        }
+
+        let leftRect = {
+            shape: "rectangle",
+            collisionClass: "0 1 2 3 4",
+            locked: true,
+            x: 0,
+            y: 0,
+            w: 10,
+            h: staticSim.box.h
+        }
+
+        let rightRect = {
+            shape: "rectangle",
+            collisionClass: "0 1 2 3 4",
+            x: 0,
+            y: staticSim.box.w,
+            w: 10,
+            locked: true,
+            h: staticSim.box.h
+        }
+
+        if(this.levels[i].bounds === 0) {
+            level.layers[5].objUniverse.push(topRect,botomRect,leftRect,rightRect);
+        }
+
+        staticSim.simulations.push(level);
+
+        for(let j = 0; j < this.levels[i].bodies.length; j++) {
+            let levelIndex = 4 - (this.levels[i].bodies[j].overlap_layer - 1);
+            level.layers[levelIndex].objUniverse.push(this.levels[i].bodies[j].phSimStaticObj); 
+        }
+
+        if(this.levels[0].gravity === true) {
+            level.world.grav = 0;
+        }
+
+        else {
+            level.world.grav = 1;
+        }
+
+    }
+
+    this.phSimStatic = staticSim;
+    this.phsim = new PhSim(staticSim);
+
+}
+
+export default createPhSimInstance;
