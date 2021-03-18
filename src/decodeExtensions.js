588aad7cf82383e325b7c9a487fef12002b87427
Michael J. Dole <dev@mjduniverse.com>
2021-03-18 00:44:03 -0500

    Decode extensions

--- src/decodeExtensions.js
+++ src/decodeExtensions.js
@@ -5,14 +5,32 @@ function decodeExtensions(extensionData) {
 
     var a = extensionData.split(";");
 
-    return {
+    var o = {
         extension: dictionary.extensions[a[0]],
         objectA: a[1],
         pointA: PPGSploderEmulator.parseVector(a[2]),
         objectB: a[3],
         pointB: PPGSploderEmulator.parseVector(a[4]),
-        radians: Number.parseFloat(a[5]),
     }
+
+    if(o.extension === "motor") {
+        o.radians = Number.parseFloat(a[5]);
+    }
+
+    if(o.extension === "arcade_mover") {
+        o.arrowKeysOnly  = !!Number.parseFloat(a[8]);
+        o.wasdKeysOnly = !!Number.parseFloat(a[9]);
+    }
+
+    if(o.extension === "mover") {
+        o.arrowKeysOnly  = !!Number.parseFloat(a[8]);
+        o.wasdKeysOnly = !!Number.parseFloat(a[9]);
+    }
+
+    if(o.extension === "adder") {
+        o.lifespan = Number.parseFloat(a[7]);
+    }
+
 
 }
 
