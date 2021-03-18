033d9084f9c15075b96e8e644dbae5b42773b6c6
Michael J. Dole <dev@mjduniverse.com>
2021-03-17 22:25:27 -0500

    Shortning code for decodeExtensions.

--- src/decodeExtensions.js
+++ src/decodeExtensions.js
@@ -1,49 +1,17 @@
-const EXTENSION_CODES = require("./extensionCodes.json");
+const PPGSploderEmulator = require(".");
+const dictionary = require("./dictionary.json");
 
 function decodeExtensions(extensionData) {
 
-    let objectA = a[1];
-    let objectB = a[3];
-
-    let pointA = a[2];
-    let pointB = a[4];
-
     var a = extensionData.split(";");
 
-    if(Number.parseInt(a[0] === EXTENSION_CODES.motor)) {
-        return {
-            extension: "motor",
-            objectA: a[1],
-            radians: Number.parseFloat(a[5])
-        }
-    }
-
-    if(Number.parseInt(a[0] === EXTENSION_CODES.bouncer)) {
-        return {
-            extension: "bouncer",
-            objectA: a[1],
-            vector: PPGSploderEmulator.parseVector(a[4])
-        }
-    }
-
-    if(Number.parseInt(a[0]) === EXTENSION_CODES.pinjoint) {
-        return {
-            extension: "pinjoint"
-        }
-    }  
-
-    if(Number.parseInt(a[0]) === EXTENSION_CODES.dragger) {
-        return {
-            extension: "dragger",
-            objectA: a[1]
-        }
-    } 
-    
-    if(Number.parseFloat(a[0]) === EXTENSION_CODES.clicker) {
-        return {
-            extension: "clicker",
-            objectA: a[1]
-        }
+    return {
+        extension: dictionary.extensions[a[0]],
+        objectA: a[1],
+        pointA: PPGSploderEmulator.parseVector(a[2]),
+        objectB: a[3],
+        pointB: PPGSploderEmulator.parseVector(a[4]),
+        radians: Number.parseFloat(a[5]),
     }
 
 }
